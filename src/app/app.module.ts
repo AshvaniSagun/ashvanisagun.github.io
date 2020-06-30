e738a7788061d905d0b4b347a14cb25798ca7816
Ashvani Kumar Sagun <AshvaniKumar.Sagun@csscorp.com>
2020-07-01 00:26:40 +0530

    First commit of covid 19 tracker

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { CountriesComponent } from './components/countries/countries.component';
+import {HttpClientModule}  from '@angular/common/http';
+import { DashboardCardComponent } from './components/dashboard-card/dashboard-card.component'
+import { GoogleChartsModule } from 'angular-google-charts';
+import { NgxSpinnerModule } from "ngx-spinner";
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    CountriesComponent,
+    DashboardCardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule, 
+    HttpClientModule, 
+    GoogleChartsModule,
+    NgxSpinnerModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
